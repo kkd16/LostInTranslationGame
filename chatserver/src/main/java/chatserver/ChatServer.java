0c3e168d05533ade4eb514957cbc3b41dfee6393
Sahba Hajihoseini <106279424+Sahbahh@users.noreply.github.com>
2023-02-19 10:22:43 -0800

    Update ChatServer.java
    The final version of the server

--- chatserver/src/main/java/chatserver/ChatServer.java
+++ chatserver/src/main/java/chatserver/ChatServer.java
@@ -104,41 +104,27 @@ class Translator {
 
 }
 
+
 class ChangeLanguage{
     static String randomizer(String message) throws IOException {
-        String[] FirstGrouplanguages = {"it", "fr", "es","ca", "de", "nl"};
-        //String[] SecondGrouplanguages = {"ko", "te", "th", "ug", "vi", "ru"};
-        String[] ThirdGrouplanguages = {"ar", "hy", "hi", "fa", "af"};
-
+       
+        //String[] FirstGrouplanguages = {"it", "fa", "ug", "es", "hy", "ca","hi", "nl","fr","de"};
+        String[] FirstGrouplanguages = {"it", "fa", "ug", "es", "hy", "ca","hi", "nl", "ml", "fr","de"};
         String tracer = "en";
 
         String translated = message;
-        for (int i = 0; i < 1; i++) {
-            for (String lang : FirstGrouplanguages) {
-                System.out.println("From: " + tracer + " to: " + lang);
-                translated = Translator.translate(tracer, lang, translated);
-                System.out.println("Translate: " + translated);
-                tracer = lang;
-                //out.println(ret);
+
+        for (String lang : FirstGrouplanguages) {
+            System.out.println("From: " + tracer + " to: " + lang);
+            translated = Translator.translate(tracer, lang, translated);
+            System.out.println("Translate: " + translated);
+            tracer = lang;
+            //out.println(ret);
             }
-            // for (String lang : SecondGrouplanguages) {
-            //     System.out.println("From: " + tracer + " to: " + lang);
-            //     translated = Translator.translate(tracer, lang, translated);
-            //     System.out.println("Translatede: " + translated);
-            //     tracer = lang;
-            //     //out.println(ret);
-            // }
-            for (String lang : ThirdGrouplanguages) {
-                System.out.println("From: " + tracer + " to: " + lang);
-                translated = Translator.translate(tracer, lang, translated);
-                System.out.println("Translatede: " + translated);
-                tracer = lang;
-                //out.println(ret);
-            }
-        }
+        
         String ret = Translator.translate(tracer, "en", translated);
         return ret;
     }
 
-
 }
+
